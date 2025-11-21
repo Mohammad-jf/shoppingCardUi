622da6125c2ac6fde5678cd825dc0f3d005da9ff
mohammad-jf <m.h.jalifathi@gmail.com>
2025-11-22 01:12:58 +0330

    add to cart function

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,19 +1,26 @@
-import React from "react";
+import { useCart } from "../context/CartContext";
 
 const ProductCard = ({ product }) => {
+  const { addToCart } = useCart();
+
   return (
-    <div
-      key={product.id}
-      className="bg-white p-4 rounded-lg shadow flex flex-col cursor-pointer"
-    >
+    <div className="bg-white rounded-lg shadow p-4 flex flex-col">
       <img
         src={product.image}
-        alt={product.title}
-        className="w-full h-40 object-cover rounded mb-4"
+        alt={product.name}
+        className="h-40 object-cover rounded mb-4"
       />
-      <h2 className="text-xl font-semibold mb-2">{product.name}</h2>
+
+      <h2 className="text-xl font-semibold">{product.name}</h2>
       <p className="text-gray-500 text-sm mb-2">{product.description}</p>
-      <p className="text-gray-800 font-bold">${product.price.toFixed(2)}</p>
+      <p className="font-bold text-lg">${product.price.toFixed(2)}</p>
+
+      <button
+        onClick={() => addToCart(product)}
+        className="bg-blue-600 hover:corsur-pointer text-white cursor-pointer mt-3 px-4 py-2 rounded transition hover:bg-blue-700"
+      >
+        Add To Cart
+      </button>
     </div>
   );
 };
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -4,9 +4,22 @@ export const CartContext = createContext();
 
 export function CartProvider({ children }) {
   const [cart, setCart] = useState([]);
+  function addToCart(product) {
+    setCart((prev) => {
+      const existing = prev.find((item) => item.id === product.id);
+
+      if (existing) {
+        return prev.map((item) =>
+          item.id === product.id ? { ...item, qty: item.qty + 1 } : item
+        );
+      }
+
+      return [...prev, { ...product, qty: 1 }];
+    });
+  }
 
   return (
-    <CartContext.Provider value={(cart, setCart)}>
+    <CartContext.Provider value={{ addToCart }}>
       {children}
     </CartContext.Provider>
   );
